5a4c8ce1fb48d01e64edab33bf325af72fce9b68
ikikika <iki.jito@gmail.com>
2021-07-01 13:03:19 +0800

    simple producer in java

--- kafka-beginner-course/src/main/java/org/example/tutorial1/ProducerDemo.java
+++ kafka-beginner-course/src/main/java/org/example/tutorial1/ProducerDemo.java
@@ -1,7 +1,33 @@
 package org.example.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String bootstrapServers = "localhost:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            // key and value serializers help producer know what kind of data we are sending to kafka
+            // and how it will be serialized to bytes
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
+
+        // send data - asynchronous
+        producer.send(record);
+        producer.flush();
+        producer.close();
     }
 }
