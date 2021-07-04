7c4914eda2db44ef296a7ad9c00e5b3f5bc8a201
ikikika <iki.jito@gmail.com>
2021-07-05 07:55:19 +0800

    complete map view

--- kafka-py-livemap/app.py
+++ kafka-py-livemap/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, request, Response
 from pykafka import KafkaClient
 
 def get_kafka_client():
@@ -6,13 +6,17 @@ def get_kafka_client():
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     return(render_template('index.html'))
+@app.route('/')
+def index():
+    return(render_template('index.html'))
 
 @app.route('/testFrontEnd')
 def testFrontEnd():
     return(render_template('test.html'))
+
+@app.route('/producer')
+def producerFrontend():
+    return(render_template('producer.html'))
 
 #Consumer API
 @app.route('/topic/<topicname>')
@@ -22,6 +26,18 @@ def get_messages(topicname):
         for i in client.topics[topicname].get_simple_consumer():
             yield 'data:{0}\n\n'.format(i.value.decode())
     return Response(events(), mimetype="text/event-stream")
+
+#Producer API
+@app.route('/topic/<topicname>', methods=['POST'])
+def send_messages(topicname):
+    # return Response(request.form['message'])
+    client = get_kafka_client()
+    producer = client.topics[topicname].get_sync_producer()
+    try:
+        producer.produce(request.form['message'].encode("ascii"))
+        return Response("success")
+    except e:
+        return Response("error")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
---
+++ kafka-py-livemap/templates/leaf.js
@@ -0,0 +1,27 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'ABCDEFG' //ENTER YOUR ACCESS TOKEN HERE
+}).addTo(mymap);
+
+mapMarkers1 = [];
+
+var source = new EventSource('/topic/TOPICNAME'); //ENTER YOUR TOPICNAME HERE
+source.addEventListener('message', function(e){
+
+  console.log('Message');
+  obj = JSON.parse(e.data);
+  console.log(obj);
+
+  if(obj.busline == '00001') {
+    for (var i = 0; i < mapMarkers1.length; i++) {
+      mymap.removeLayer(mapMarkers1[i]);
+    }
+    marker1 = L.marker([obj.latitude, obj.longitude]).addTo(mymap);
+    mapMarkers1.push(marker1);
+  }
+
+
+}, false);
